a07c8f2f69390afca561d87b5ac94af5ea1e8531
kodandaram199 <kodandaram199@missionrnd.com>
2016-03-31 21:32:19 +0530

    Submit Commit

--- src/Maze.cpp
+++ src/Maze.cpp
@@ -35,8 +35,41 @@ more parameters .
 
 #include<stdlib.h>
 
+int PathExists(int *maze, int *res, int columns,int rows, int x1, int y1, int x2, int y2){
+	if (x1 < 0 || y1 < 0 || x1 >= rows || y1 >= columns|| maze[(columns*x2) + y2] == 0 || maze[(columns*x1) + y1] == 0)
+		return 0;
+
+	if (x1 == x2 && y1 == y2)
+		return 1;
+
+	if (maze[(columns*x1) + y1] == 1 && res[(columns*x1) + y1] == 0){
+
+		res[(columns*x1) + y1] = 1;
+
+		if (PathExists(maze, res, columns, rows, x1 - 1, y1, x2, y2) == 1)
+			return 1;
+		if (PathExists(maze, res, columns, rows, x1+1, y1, x2, y2) == 1)
+			return 1;
+		if (PathExists(maze, res, columns, rows, x1, y1-1, x2, y2) == 1)
+			return 1;
+		if (PathExists(maze, res, columns, rows, x1, y1 + 1, x2, y2) == 1)
+			return 1;
+		return 0;
+	}
+
+	return 0;
+}
 
 int path_exists(int *maze, int rows, int columns, int x1, int y1, int x2, int y2)
 {
-	return 1;
+
+	if (rows <= 0 || columns <= 0 ||x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0 || x1 >= rows || x2 >= rows || y1 >= columns || y2 >= columns)
+		return 0;
+	int *res = (int*)malloc((rows*columns)*sizeof(int));
+	for (int i = 0; i < rows*columns; i++)
+		res[i] = 0;
+
+	int result = PathExists(maze, res, columns, rows, x1, y1, x2, y2);
+
+	return result;
 }
--- src/Spiral.cpp
+++ src/Spiral.cpp
@@ -1,40 +1,87 @@
 /*
 Write a program to generate elements of a matrix in spiral order.
 Store the generated elements in single dimensional array and return that array.
-
 Directions for Spiral are : Go Right ,Go Down ,Go Left, Go Up
 Ex:
-
 Matrix:
 1	2	3
 4	5	6
 7	8	9
-
 Spiral order:
 1	2	3	6	9	8	7	4	5
-
 Given matrix is a two dimensional array(input_array) with dimensions rows, columns.
 If rows, columns are invalid then return NULL.
 If input_array is NULL then return NULL.
-
 Example Input :
 int board[2][3]={
-				 {4,6,7},
-				 {1,3,8}
-				};
+{4,6,7},
+{1,3,8}
+};
 spiral(2,2,(int **)board); // Rows followed by columns followed by board
-
 Example Output : Return an array consisting of {4,6,7,8,3,1};
-
 Note : Check the function Parameters ,Its a double pointer .
-
 */
 
 
 #include "stdafx.h"
 #include<stdlib.h>
 
+void spiralRec(int rows, int columns, int **input_array, int *CurrentRow, int *CurrentColumn, int *a, int *index){
+	int i, cr = *CurrentRow, cc = *CurrentColumn;
+	if (cr >= rows || cc >= columns){
+		return;
+	}
+	for (i = cc; i < columns; ++i)
+	{
+		a[(*index)++] = input_array[cr][i];
+	
+	}
+	cr++;
+
+	for (i = cr; i < rows; ++i)
+	{
+		a[(*index)++] = input_array[i][columns - 1];
+
+	}
+	columns = columns - 1;
+
+	if (cr < rows)
+	{
+		for (i = columns - 1; i >= cc; --i)
+		{
+			a[(*index)++] = input_array[rows - 1][i];
+
+		}
+		rows = rows - 1;
+	}
+	else
+	{
+		return;
+	}
+
+	if (cc < columns)
+	{
+		for (i = rows - 1; i >= cr; --i)
+		{
+			a[(*index)++] = input_array[i][cc];
+
+		}
+		cc++;
+	}
+	else
+	{
+		return;
+	}
+	*CurrentRow = cr;
+	*CurrentColumn = cc;
+	spiralRec(rows, columns, input_array, CurrentRow, CurrentColumn, a, index);
+}
 int *spiral(int rows, int columns, int **input_array)
 {
-	return NULL;
+	if (input_array == NULL || rows <= 0 || columns <= 0)
+		return NULL;
+	int index = 0, CurrentRow = 0, CurrentColumn = 0;
+	int *res = (int *)malloc(sizeof(int)*(rows*columns));
+	spiralRec(rows, columns, input_array, &CurrentRow, &CurrentColumn, res, &index);
+	return res;
 }
